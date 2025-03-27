485529d582942a07430c335042dedad51899f330
glapettina <guillermo@glsoftware.com.ar>
2025-03-27 19:22:21 -0300

    erased views.py from core

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .views import TestView
 
 urlpatterns = [
     path('test/', TestView.as_view()),
